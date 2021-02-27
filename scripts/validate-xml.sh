06186d83d426c1dcf386b159596ac953d5e0ce56
Tom Gilissen <tomgilissen@users.noreply.github.com>
2021-02-27 08:40:07 +0100

    Added XML validation job (#32)
    To make sure all configuration files are valid XML, a XML validation test has been added. The test will run as on each update of the repo.

---
+++ scripts/validate-xml.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# validate-xml.sh : Script to validate all XML files in the DWCA repository
+
+ERRORS=false
+
+# Search for all XML files in the specimen and taxon directory
+XML_FILES=( `find {./specimen/,./taxon/} -name \*.xml` )
+
+# Validate all XML files
+for file in ${XML_FILES[@]}; do
+  /usr/bin/xmllint --noout $file
+  if [ $? -gt 0 ]; then
+      ERRORS=true;
+  fi
+done
+
+if $ERRORS; then
+  exit 1
+fi
+
